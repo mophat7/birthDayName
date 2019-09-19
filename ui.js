54f5469c784cc6479f1b11f8ddec99292561e501
mophat7 <watalcomcentre@gmail.com>
2019-09-19 07:50:52 +0300

    Create variables to hold data from user input

--- ui.js
+++ ui.js
@@ -1,5 +1,6 @@
-$(function(){
-    $('#date').datepicker();
-});
-
-document.querySelector('')
+//Create variables
+const day= document.getElementById('day');
+const month= document.getElementById('month');
+const year= document.getElementById('year');
+const form=document.getElementById('form');
+//function to check errors on user input
